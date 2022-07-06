3745e41426753360639cc62cf1714913b2296289
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2022-07-06 20:01:42 +0300

    Time: 53 ms (5.22%), Space: 13.6 MB (14.66%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        
+        """
+        
+        
+        for i in range(len(nums)):
+            for j in range(len(nums)-1):
+                if nums[j+1] < nums[j]:
+                    nums[j+1],nums[j] = nums[j],nums[j+1]
+                else:
+                    continue
+        
+        
+        
