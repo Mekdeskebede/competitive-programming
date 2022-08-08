9ce0bb0dcaa970c7a92d70a311580257684f47a7
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2022-08-08 19:16:21 +0300

    Time: 113 ms (35.71%), Space: 18.1 MB (39.53%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,35 +1,37 @@
-class MinStack(object):
+class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_stack =[]
 
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
+    def push(self, val: int) -> None:
         self.stack.append(val)
+        if self.min_stack and self.min_stack[-1]>= val:
+            self.min_stack.append(val)
+        elif self.min_stack==[]:
+            self.min_stack.append(val)
+        print(self.min_stack)
+
+            
+                
+
+    def pop(self) -> None:
+        popped = self.stack.pop()
+        if popped== self.min_stack[-1]:
+            self.min_stack.pop(-1)
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
         
         
 
-    def pop(self):
-        """
-        :rtype: None
-        """
-        return self.stack.pop(-1)
+    def getMin(self) -> int:
+        # print(self.min_stack)
+        if self.min_stack:
+            return self.min_stack[-1]
         
-
-    def top(self):
-        """
-        :rtype: int
-        """
-        return self.stack[-1]
-
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return min(self.stack)
         
 
 
