3629dbf0a551376bd38a36547e424badbcf8be59
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2022-09-15 19:56:20 +0300

    Time: 350 ms (5.03%), Space: 18.9 MB (35.44%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,28 +1,21 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        frequency = defaultdict(int)
-    
-    
+        freq = defaultdict(int)
         for i in nums:
-            frequency[i] +=1
-
-        value_list= list(frequency.values())
-        value_list.sort()
-        value_list.reverse()
-        final=[]
-        for i in value_list[:k]:
-            for keys, values in frequency.items():
-                if  values==i:
-                    final.append(keys)
-                    del frequency[keys]
-                    break
-
-        return final
+            if i in freq:
+                 freq[i] += 1
+            else:
+                freq[i] = 1
+                
+        freq_lst = []
+        for i in freq:
+            heapq.heappush(freq_lst, (-freq[i],i))
+       
+        ans = []
+        for i in range(k):
+            ans.append(heapq.heappop(freq_lst)[1])
             
+        return ans
+  
             
