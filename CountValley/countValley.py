59ff58824b3c9370598ab565dd019cec756d7227
Mekdes Kebede <kebedemekdes289@gmail.com>
2022-09-29 12:14:47 +0300

    count valley

---
+++ CountValley/countValley.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    seaLevel = 0
+    valley = 0
+    
+    for i in path:
+        if i == "U":
+            if seaLevel + 1 == 0:
+                valley += 1
+            seaLevel += 1
+        else:
+            seaLevel -= 1
+    return valley
+                
+            
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
