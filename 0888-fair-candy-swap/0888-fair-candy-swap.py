520b0e64f828560c628b35d03f579d977be08d95
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2022-11-15 11:40:16 +0300

    Time: 8750 ms (5.97%), Space: 16 MB (90.59%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.py
+++ 0888-fair-candy-swap/0888-fair-candy-swap.py
@@ -1,9 +1,7 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         diff = (sum(bobSizes)- sum(aliceSizes))/2
-        alice = set(aliceSizes)
-        
         for bob in bobSizes:
-            if bob - diff in alice:
+            if bob - diff in aliceSizes:
                 return [int(bob - diff ) , bob]
             
