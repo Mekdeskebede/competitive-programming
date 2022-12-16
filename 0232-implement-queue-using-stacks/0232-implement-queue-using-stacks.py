031980fc5e4dfd8f7530a715d774605e86d184d2
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2022-12-16 17:37:27 +0300

    Time: 25 ms (98.54%), Space: 13.9 MB (98.86%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,41 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop(0)
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        if len(self.stack) ==0:
+            return True
+        else: 
+            return False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
