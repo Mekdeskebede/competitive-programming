5225fa93177112306cfa5b8352a4be17160c072f
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-01-21 23:09:20 +0300

    Time: 43 ms (61.76%), Space: 13.8 MB (79.91%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = [] 
+        if len(s) > 12: 
+            return
+        def BT(i, dots, curIP): 
+            if dots == 4 and i == len(s): 
+                res.append(curIP[:-1]) 
+                return
+            if dots > 4:
+                return 
+            for j in range(i, min(i + 3, len(s))): 
+                if int(s[i:j+1]) < 256 and (i==j or s[i]!= "0"): 
+                    BT(j + 1, dots + 1, curIP + s[i: j+1] +".") 
+        BT(0, 0, "")
+        return res
