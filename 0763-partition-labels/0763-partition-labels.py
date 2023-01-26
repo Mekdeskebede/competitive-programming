ddb59161b0325c3e8af75f814e9df4ba2a5ce3ce
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-01-26 17:45:13 +0300

    Time: 41 ms (76.87%), Space: 13.8 MB (97.36%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,21 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         
-        letter = defaultdict()
-        for idx, val in enumerate(s):
-            letter[val] = idx
+        n = len(s)
+        last_occurance = {s[index]:index for index in range(n)}
         
-        count = 0
-        stop = 0
-        res = []
-        for i,char in enumerate(s):
-            count += 1
-            if letter[char] > stop:
-                stop = letter[char]
-            if stop == i:
-                res.append(count)
-                count = 0
-        return res
-                
+        parts = []
+        last = 0
+        prev = -1
+        
+        for i in range(n):
             
+            last = max(last,last_occurance[s[i]])
+            if last == i:
+                parts.append(last - prev)
+                prev = last 
             
+        return parts
+        
