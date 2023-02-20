93c8910f3dddbd9547012e6d056b3be2f66a7869
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-02-20 16:29:18 +0300

    Time: 34 ms (54.23%), Space: 13.8 MB (46.85%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -8,20 +8,47 @@ class Solution:
         
         # approach one
         
-        curr = head
-        length = 0
+#         curr = head
+#         length = 0
         
-        while curr:
-            curr = curr.next 
-            length += 1
+#         while curr:
+#             curr = curr.next 
+#             length += 1
             
-        mid = length//2
-        curr = head
+#         mid = length//2
+#         curr = head
         
-        while mid:
-            curr = curr.next
-            mid -= 1
+#         while mid:
+#             curr = curr.next
+#             mid -= 1
         
-        return curr
-#     
+#         return curr
+    
+#     approach two
+
+        
+#         curr = head
+#         arr = []
+        
+#         while curr:
+#             arr.append(curr)
+#             curr = curr.next 
+            
+#         return arr[len(arr)//2]
+    
+#     approach 3
+
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
+            
+
+        
+
         
