fed98eae808d2f866e0c80110c65fe1695995455
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-02-28 16:41:22 +0300

    Time: 33 ms (62.11%), Space: 13.8 MB (51.80%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,14 +1,28 @@
 class Solution:
+    def __init__(self):
+        self.res = []
     def getRow(self, rowIndex: int) -> List[int]: 
-        table = [[0] * (i + 1) for i in range(0, rowIndex + 1)]
-        table[0] = [1]
-        for i in range(1, rowIndex + 1):
-            for j in range(len(table[i])):
-                if j == 0 or j == len(table[i]) - 1:
-                    table[i][j] = 1
-                else:
-                    table[i][j] = table[i - 1][j - 1] + table[i - 1][j]
-        return table[-1]
+        def recur(n,path,rowIndex):
+        
+            if rowIndex == -1:
+                self.res = path.copy()
+                return 
+            if not path:
+                ans = [1]
+            elif path == [1]:
+                ans = [1,1]
+            else:
+                ans = [0] * (n+1)
+                for i in range(n+1):
+                    if i == 0 or i == n:
+                        ans[i] = 1
+                    else:
+                        ans[i] = path[i-1] + path[i]
+            recur(n + 1, ans,rowIndex - 1)
+            
+            
+        recur(0,[],rowIndex)
+        return self.res
         
         
         
