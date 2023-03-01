8fcda18294e97e03ff64e4e519c022c29b9768b0
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-03-01 21:22:55 +0300

    Time: 113 ms (86.53%), Space: 14 MB (51.29%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -8,20 +8,19 @@ class Solution:
         longest = 0
         max_len = 0
         string = defaultdict(int)
-         
-        # print(string)
+        left = 0
+        
         for right in range(n):
             string[s[right]] += 1
             longest = max(longest,string[s[right]])
-            print(string)
             
-            if max_len - longest >= k:
-                string[s[right - max_len]] -= 1
-                # continue
-            else:
-                max_len += 1
-            
-
+            while (right-left) - longest >= k:
+                string[s[left]] -= 1
+                if string[s[left]] == 0:
+                    del string[s[left]]
+                left += 1
+                
+            max_len = max(max_len, right-left+1)
         
         return max_len
             
