9eb5a15d2bfa4d0808492246b11f99d3408a7df3
Mekdes Kebede <kebedemekdes289@gmail.com>
2023-03-02 22:45:03 +0300

    recursion

---
+++ recursiveDigitSum/recursive_digit_sum.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+    p = 0
+    for dgt in n:
+        p += int(dgt)
+    p *= k
+    p = str(p)
+    
+    while len(p) > 1:
+        temp = 0
+        for dgt in p:
+            temp += int(dgt)
+        p = str(temp)
+    return int(p)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
