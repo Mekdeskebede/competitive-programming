096777a4aa2d557944c3a0f10210f12e5631cf2b
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-03-22 22:33:31 +0300

    Time: 320 ms (70.22%), Space: 19.2 MB (17.04%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -6,38 +6,22 @@
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
         
-        given = []
         stack = []
-        res = []
-        
+        ans = {}
+        length = 0
         
         while head:
-            given.append(head.val)
-            res.append(0)
+            
+            while stack and stack[-1][1] < head.val:
+                idx, val = stack.pop()
+                ans[idx] = head.val
+            stack.append([length,head.val])
             head = head.next
-            
-        for i in range(len(given)):
-            
-            while stack and stack[-1][1] < given[i]:
-                indx, val = stack.pop()
-                res[indx] = given[i]
-                
-            stack.append((i,given[i]))
+            length += 1
+        
+        res = [0] * length
+       
+        for key,val in ans.items():
+            res[key] = val
         
         return res
-#         while head:
-#             second = head.next
-#             temp = head.next
-            
-#             while temp  and temp.val <= head.val:
-#                 temp = temp.next
-                
-#             if temp == None:
-#                 stack.append(0)
-#             else:
-#                 stack.append(temp.val)
-                
-    
-#             head = second
-            
-        return stack
