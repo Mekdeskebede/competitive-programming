9b12d4f4acb2e9a209e55af4b12ecee55a8b5a90
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-03-24 17:52:20 +0300

    Time: 73 ms (93.35%), Space: 14.1 MB (43.48%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -1,27 +1,3 @@
-# class Solution:
-#     def largestMerge(self, str1: str, str2: str) -> str:
-#         n, m = len(str1), len(str2)
-#         i = j = 0
-#         res = ""
-#         while i < n and j < m:
-#             if str1[i] > str2[j]:
-#                 res += str1[i]
-#                 i += 1
-#             elif str1[i] < str2[j]:
-#                 res += str2[j]
-#                 j += 1
-#             else:
-#                 if str1[i:] > str2[j:]:
-#                     res += str1[i]
-#                     i += 1
-#                 else:
-#                     res += str2[j]
-#                     j += 1
-
-#         return res + str1[i:] + str2[j:]
-
-
-
 class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
         ptr1, ptr2 = 0, 0
