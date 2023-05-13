7e9a4277683d8ca1938b382e52c6ac4f123fd354
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-05-13 07:14:16 +0300

    Time: 690 ms (44.00%), Space: 24.1 MB (24.64%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,33 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        adj_list = defaultdict(list)
+        incoming = []
+        n = len(graph)
+        ans = []
+        queue = deque()
+        
+        for i in range(n):
+            incoming.append(len(graph[i]))
+            if incoming[i] == 0:
+                queue.append(i)
+            for outgoing in graph[i]:
+                adj_list[outgoing].append(i)
+                
+        while queue:
+            node = queue.popleft()
+            for neighbor in adj_list[node]:
+                incoming[neighbor] -= 1
+                if incoming[neighbor] == 0:
+                    queue.append(neighbor)
+                    ans.append(neighbor)
+                    
+        for i in range(n):
+            if len(graph[i]) == 0:
+                ans.append(i)
+        ans.sort()
+        return ans
+                    
+                    
+                    
+            
+        
