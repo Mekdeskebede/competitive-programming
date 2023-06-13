2e4622d06e83a97a4b45669deb3c41a216f552d3
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-06-13 22:49:07 +0300

    Time: 43 ms (79.33%), Space: 16.1 MB (70.92%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,7 +1,4 @@
 class Solution:
     def minOperations(self, n: int) -> int:
         
-        ans = 0
-        for i in range(n//2,n):
-            ans += (2*i+1) - n
-        return ans
+        return (n*n)//4
