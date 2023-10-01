4d94c7a50e5f5ef3ed55be8078ab9fe81b7cfaf1
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-10-01 10:47:43 +0300

    Time: 270 ms (8.22%), Space: 18 MB (11.04%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,17 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None for _ in range(26)]
+        self.is_end = False
+
 class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+        
     def longestWord(self, words: List[str]) -> str:
-        longest = ""
-        word_set = set(words)
+        for word in words:
+            self.insert(word)
+        ans = ""
         words.sort()
         for word in words:
-            if len(word) > len(longest):
-                isPossible = True
-                for i in range(1,len(word)):
-                    if word[:i] not in word_set:
-                        isPossible = False
-                        break
-                if isPossible:
-                    longest = word
-        return longest
+            temp = self.dfs(word)
+            if len(ans) < len(temp):
+                ans = temp
+        return ans
+        
+    def insert(self,word):
+        curr = self.root
+        for char in word:
+            if not curr.children[ord(char) - 97]:
+                curr.children[ord(char) - 97] = TrieNode()
+            curr = curr.children[ord(char) - 97]
+        curr.is_end = True
+    def dfs(self,word):
+        curr = self.root
+        for i in range(len(word)-1):
+            if not curr.children[ord(word[i])-97] or not curr.children[ord(word[i])-97].is_end:
+                return ""
+            curr = curr.children[ord(word[i])-97]
+            
+        return word
+                
+            
                 
                 
