281a0e3ebf8dd5aa52991307366feab856c32b03
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-10-09 10:57:41 +0300

    Time: 47 ms (5.98%), Space: 16.2 MB (82.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,14 +1,27 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)):
-            temp = i
-            for j in range(len(needle)):
-                if temp < len(haystack) and haystack[temp] == needle[j]:
-                    j += 1
-                    temp +=1 
-                else:
-                    break
-            if j == len(needle):
-                return i
+        
+        n_hash = 0
+        curr_hash = 0
+        n = len(haystack)
+        m = len(needle)
+        if m > n:
+            return -1
+        a = 27
+        for i in range(m):
+            n_hash += (a**(m-i-1) * (ord(needle[i])-96))
+            curr_hash += (a**(m-i-1) * (ord(haystack[i])-96))
+            
+        if curr_hash == n_hash:
+            return 0
+        left = 0
+        for right in range(m,n):
+            curr_hash -= ((ord(haystack[left])-96)*(a**(m-1)))
+            left += 1
+            curr_hash *= a
+            curr_hash += (ord(haystack[right])-96)
+            if curr_hash == n_hash:
+                return left
+            
         return -1
         
