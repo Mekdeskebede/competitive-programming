89bcf629d96f1605403bff16c8740e6d406610e6
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-10-20 09:10:05 +0300

    Time: 37 ms (69.23%), Space: 16.2 MB (77.53%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        if n%2==0:
+            return (pow(5,n//2,10**9+7)) * (pow(4,n//2,10**9+7)) % (10**9+7)
+        return (pow(5,n//2+1,10**9+7) * pow(4,n//2,10**9+7)) % (10**9+7)
