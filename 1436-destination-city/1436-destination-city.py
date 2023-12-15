afa9a903d0bde2525306dc06111ecb420993d4db
Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
2023-12-15 11:11:45 +0300

    Time: 56 ms (87.39%), Space: 16.3 MB (45.28%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,12 +1,10 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        adj_list = defaultdict()
+        
         cities = set()
         for path in paths:
-            adj_list[path[0]] = path[1]
             cities.add(path[0])
-            cities.add(path[1])
             
-        for city in cities:
-            if city not in adj_list:
-                return city
+        for city in paths:
+            if city[1] not in cities:
+                return city[1]
